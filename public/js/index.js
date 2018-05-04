a605134d27237233de6a38fc8de1eab7dd29f659
Ashutosh3436 <ashu.python@gmail.com>
2018-05-05 01:36:21 +0530

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on('connect', function() {
     console.log('Connected to the server');
 
-    socket.emit('createMessage', {
-        from:'jen@example.com',
-        text:'Hey, this is ashu'
-    });
+    // socket.emit('createMessage', {
+    //     from:'jen@example.com',
+    //     text:'Hey, this is ashu'
+    // });
 });
 
 socket.on('disconnect', function() {
